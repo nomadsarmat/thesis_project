1ea0a8b1a9393888deb7db6d798f9531d6593afa
Usenko Jaroslav <usenko.jaroslav@gmail.com>
2015-04-28 16:58:04 +0300

    Создан django проэкт и приложение watermark

---
+++ thesis_project/watermark/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+
+urlpatterns = [
+    # Create your urls here.
+    # Examples:
+    # url(r'^$', 'thesis_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+]
